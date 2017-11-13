4e40b1f4f258bef1c990c48c5b049b42228d7fd6
Doug <dougwright1@gmail.com>
2017-11-13 11:04:54 -0500

    add socket as opt
    remove w3cwebsocket - not ssr in my app...

--- src/PhoenixWebSocketLink.js
+++ src/PhoenixWebSocketLink.js
@@ -1,49 +1,50 @@
-import { print } from 'graphql/language/printer'
-import { Observable } from 'apollo-link-core'
-import { Socket as PhoenixSocket } from 'phoenix'
-import { w3cwebsocket as W3CWebSocket } from 'websocket'
+import { print } from "graphql/language/printer";
+import { Observable } from "apollo-link-core";
+import { Socket as PhoenixSocket } from "phoenix";
+//import { w3cwebsocket as W3CWebSocket } from "websocket";
 
 export class PhoenixWebSocketLink {
-  constructor (opts) {
-    if (typeof window === 'undefined' && !opts.transport) opts.transport = W3CWebSocket
-    let socket = new PhoenixSocket(opts.uri, opts)
+  constructor(opts) {
+    this._socket = !opts.socket
+      ? new PhoenixSocket(opts.uri, opts)
+      : opts.socket;
 
     try {
-      socket.connect()
-      this._joinChannel(socket)
+      this._socket.connect();
+      this._joinChannel();
     } catch (err) {
-      console.error(err)
+      console.error(err);
     }
   }
 
   // Required by the NetworkInterface interface spec
   // param: { variables, extensions, operationName, query }
-  request ({ operationName, query, variables }) {
-    query = print(query)
-		// console.log('Phoenix apollo: operationName:', operationName)
-		return new Observable(observer => {
-			this._channel
-				.push('doc', { operationName, query, variables })
-				.receive('ok', response => {
+  request({ operationName, query, variables }) {
+    query = print(query);
+    // console.log('Phoenix apollo: operationName:', operationName)
+    return new Observable(observer => {
+      this._channel
+        .push("doc", { operationName, query, variables })
+        .receive("ok", response => {
           // console.log('operation response', response)
-          observer.next(response)
-          observer.complete()
+          observer.next(response);
+          observer.complete();
         })
-				.receive('ignore', response => {
-          observer.next(response)
-          observer.complete()
+        .receive("ignore", response => {
+          observer.next(response);
+          observer.complete();
         })
-				.receive('error', observer.error.bind(observer))
-				.receive('timeout', observer.error.bind(observer))
-		})
+        .receive("error", observer.error.bind(observer))
+        .receive("timeout", observer.error.bind(observer));
+    });
   }
 
-  _joinChannel (socket) {
-		const CHANNEL_TOPIC = '__absinthe__:control'
-		let channel = socket.channel(CHANNEL_TOPIC, {})
-		this._channel = channel
-    channel.join()
-      // .receive('ok', response => console.log(`Joined successfully ${CHANNEL_TOPIC}`, response))
-      // .receive('error', response => { console.log(`Unable to join ${CHANNEL_TOPIC}`, response) })
+  _joinChannel() {
+    const CHANNEL_TOPIC = "__absinthe__:control";
+    let channel = this._socket.channel(CHANNEL_TOPIC, {});
+    this._channel = channel;
+    channel.join();
+    // .receive('ok', response => console.log(`Joined successfully ${CHANNEL_TOPIC}`, response))
+    // .receive('error', response => { console.log(`Unable to join ${CHANNEL_TOPIC}`, response) })
   }
 }
